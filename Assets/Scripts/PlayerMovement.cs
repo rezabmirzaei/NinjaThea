fec0f6147a3b1dee7c584f29c97abfcf4797885b
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-04-17 13:28:32 +0200

    Movement & jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,37 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
-    public CharacterController2D controller;
-    public Animator animator;
-    public float runSpeed = 40f;
+    private float horizontalMove = 0f;
+    private bool jump = false;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+    [SerializeField] private LayerMask terrain;
 
-    float horizontalMove = 0f;
-    bool jump = false;
+    private enum MovementState { idle, running, jumping, falling }
 
-    void Update()
+    // Start is called before the first frame update
+    private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
 
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    // Update is called once per frame
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
 
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded())
         {
             jump = true;
-            animator.SetBool("IsJumping", true);
         }
 
+        UpdateAnimationState();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
-        jump = false;
+        rb.velocity = new Vector2(horizontalMove * moveSpeed, rb.velocity.y);
+        if (jump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jump = false;
+        }
     }
 
-    public void OnLoanding()
+    private void UpdateAnimationState()
     {
-        animator.SetBool("IsJumping", false);
+        MovementState state;
+
+        if (horizontalMove > 0f)
+        {
+            state = MovementState.running;
+            spriteRenderer.flipX = false;
+        }
+        else if (horizontalMove < 0f)
+        {
+            state = MovementState.running;
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        animator.SetInteger("State", (int)state);
     }
+
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, terrain);
+    }
+
 }
