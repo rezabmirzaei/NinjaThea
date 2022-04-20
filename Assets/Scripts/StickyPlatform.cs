1e10fcf5c7da1e14abfbeb5c0a6b7ac4236cced1
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-04-20 14:22:22 +0200

    Moving platform

---
+++ Assets/Scripts/StickyPlatform.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+
+}
---
+++ Assets/Scripts/WaypointHandler.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class WaypointHandler : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] waypoints;
+    [SerializeField] private float speed = 2f;
+    private int currentWaypointIndex = 0;
+
+    private void Update()
+    {
+
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+
+    }
+}
