3c5439e1243811aabb8dfbdab0c68a1700273d14
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-04-23 23:37:52 +0200

    Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
