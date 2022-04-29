7e32a358a95b18aed5d3454e550bed091229f193
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-04-29 20:53:07 +0200

    End Scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Level 1");
     }
 
     public void QuitGame()
