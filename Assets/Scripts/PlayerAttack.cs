9e952dabfc3f1b584504d6a3340a647098624277
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-05-08 14:46:51 +0200

    Enemy/hit detection (die) - prefab

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -34,7 +34,7 @@ public class PlayerAttack : MonoBehaviour
 
         foreach (Collider2D enemy in hitEnemies)
         {
-            Debug.Log(enemy.name);
+            enemy.GetComponent<Enemy>().Die();
         }
     }
 
