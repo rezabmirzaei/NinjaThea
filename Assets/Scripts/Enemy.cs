1dbf66f0876d82ca5cd37f6ca05c1e33cf1617e3
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-05-12 23:03:58 +0200

    Enemy patrol.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,19 @@ public class Enemy : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private Collider2D coll;
 
+    private bool isDead = false;
+
     public void Die()
     {
         GameController.Instance.EnemyKilled();
+        isDead = true;
         animator.SetTrigger("Death");
         coll.enabled = false;
         this.enabled = false;
+    }
+
+    public bool IsDead()
+    {
+        return isDead;
     }
 }
---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [Header("Patrol points")]
+    [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
+
+    [Header("Enemy")]
+    [SerializeField] private Transform enemy;
+    [SerializeField] private float speed = 2;
+
+    private Vector3 initScale;
+    private bool movingLeft;
+
+    private void Awake()
+    {
+        initScale = enemy.localScale;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!enemy.GetComponent<Enemy>().IsDead())
+        {
+            if (movingLeft)
+            {
+                if (enemy.position.x >= leftEdge.position.x) Patrol(-1);
+                else ChangeDirection();
+            }
+            else
+            {
+                if (enemy.position.x <= rightEdge.position.x) Patrol(1);
+                else ChangeDirection();
+            }
+        }
+    }
+
+    private void Patrol(int direction)
+    {
+        enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * direction, initScale.y, initScale.z);
+        enemy.position = new Vector3(enemy.position.x + Time.deltaTime * direction * speed, enemy.position.y, enemy.position.z);
+    }
+
+    private void ChangeDirection()
+    {
+        movingLeft = !movingLeft;
+    }
+
+}
