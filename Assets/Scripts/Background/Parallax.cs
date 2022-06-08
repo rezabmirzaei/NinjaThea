89441ebd14529eb23830e4dda3849433ebee8416
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-06-08 21:29:48 +0200

    Parallax effect, repeat bg.

--- Assets/Scripts/Background/Parallax.cs
+++ Assets/Scripts/Background/Parallax.cs
@@ -17,7 +17,11 @@ public class Parallax : MonoBehaviour
 
     private void Update()
     {
+        float temp = cam.transform.position.x * (1 - parallaxEffect);
         float dist = cam.transform.position.x * parallaxEffect;
         transform.position = new Vector3(startPos + dist, .0f, transform.position.z);
+
+        if (temp > startPos + length) startPos += length;
+        else if (temp < startPos - length) startPos -= length;
     }
 }
